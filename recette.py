ac29a9a19ebfadcf9100a3ed7c260f535621cf5d
prpletrtle <alexandre.nouhaud@gmail.com>
2019-07-23 17:56:50 +0200

    show list ingredients

--- recette.py
+++ recette.py
@@ -7,8 +7,9 @@ from PyQt5 import QtWidgets, uic
 
 listMesures = ['cuillère à café','cuillères à café', 'c.c.', 'cuillère à dessert','cuillères à dessert', 'cuillère à soupe','cuillères à soupe',
                  'c.s.', 'tasse à café','tasses à café', 'bol','bols', 'verre à moutarde','verres à moutarde',
-                 'verre à liqueur', 'grand verre', 'gallon', 'tasse', 'pincée', 'grammes',' g)','g ',' g ', 'litres', ' l ', ' cl', ' ml', ' dl', 'botte']
-listeArticles = [" de ", " d’", '(',')', '®', '%']
+                 'verre à liqueur', 'grand verre', 'gallon', 'tasse', 'pincée', 'grammes',' g)','g ',' g ', 'litres', ' l ', ' cl', ' ml', ' dl', 'botte',
+                 'kilo ','demi ', 'verre ','m.g.','poignée ','grosse ','petite ','petit ', 'quelques ','brins ','environ ','morceau ','quart ','demi ','trait ','petites ']
+listeArticles = [" de ", " d’", '(',')', '®', '%',' le ', 'un ',' d ']
 listeCourses = []
 
 
@@ -23,6 +24,19 @@ class Yara(QtWidgets.QMainWindow):
         
         #gérer les inputs text
         self.lundiMidi = self.findChild(QtWidgets.QLineEdit, 'lundiMidi')
+        self.lundiSoir = self.findChild(QtWidgets.QLineEdit, 'lundiSoir')
+        self.mardiMidi = self.findChild(QtWidgets.QLineEdit, 'mardiMidi')
+        self.mardiSoir = self.findChild(QtWidgets.QLineEdit, 'mardiSoir')
+        self.mercrediMidi = self.findChild(QtWidgets.QLineEdit, 'mercrediMidi')
+        self.mercrediSoir = self.findChild(QtWidgets.QLineEdit, 'mercrediSoir')
+        self.jeudiMidi = self.findChild(QtWidgets.QLineEdit, 'jeudiMidi')
+        self.jeudiSoir = self.findChild(QtWidgets.QLineEdit, 'jeudiSoir')
+        self.vendrediMidi = self.findChild(QtWidgets.QLineEdit, 'vendrediMidi')
+        self.vendrediSoir = self.findChild(QtWidgets.QLineEdit, 'vendrediSoir')
+        self.samediMidi = self.findChild(QtWidgets.QLineEdit, 'samediMidi')
+        self.samediSoir = self.findChild(QtWidgets.QLineEdit, 'samediSoir')
+        self.dimancheMidi = self.findChild(QtWidgets.QLineEdit, 'dimancheMidi')
+        self.dimancheSoir = self.findChild(QtWidgets.QLineEdit, 'dimancheSoir')
 
         #gerer la liste des ingredients
         self.ingredients = self.findChild(QtWidgets.QListWidget, 'listIngr')
@@ -30,10 +44,55 @@ class Yara(QtWidgets.QMainWindow):
         self.show() # Show the GUI
     
     def validRecettePressed(self):
-        print('Recette lundi : '+ self.lundiMidi.text())
-        self.ingredients.addItem(self.lundiMidi.text())
+        urlLundiM = self.lundiMidi.text()
+        urlLundiS = self.lundiSoir.text()
+        urlMardiM = self.mardiMidi.text()
+        urlMardiS = self.mardiSoir.text()
+        urlMercrediM = self.mercrediMidi.text()
+        urlMercrediS = self.mercrediSoir.text()
+        urlJeudiM = self.jeudiMidi.text()
+        urlJeudiS = self.jeudiSoir.text()
+        urlVendrediM = self.vendrediMidi.text()
+        urlVendrediS = self.vendrediSoir.text()
+        urlSamediM = self.samediMidi.text()
+        urlSamediS = self.samediSoir.text()
+        urlDimancheM = self.dimancheMidi.text()
+        urlDimancheS = self.dimancheSoir.text()
 
-    def formatString(nomString):
+        if urlLundiM:
+            self.FindIngredient(urlLundiM)
+        if urlLundiS:
+            self.FindIngredient(urlLundiS)
+        if urlMardiM:
+            self.FindIngredient(urlMardiM)
+        if urlMardiS:
+            self.FindIngredient(urlMardiS)
+        if urlMercrediM:
+            self.FindIngredient(urlMercrediM)
+        if urlMercrediS:
+            self.FindIngredient(urlMercrediS)
+        if urlJeudiM:
+            self.FindIngredient(urlJeudiM)
+        if urlJeudiS:
+            self.FindIngredient(urlJeudiS)
+        if urlVendrediM:
+            self.FindIngredient(urlVendrediM)
+        if urlVendrediS:
+            self.FindIngredient(urlVendrediS)
+        if urlSamediM:
+            self.FindIngredient(urlSamediM)
+        if urlSamediS:
+            self.FindIngredient(urlSamediS)
+        if urlDimancheM:
+            self.FindIngredient(urlDimancheM)
+        if urlDimancheS:
+            self.FindIngredient(urlDimancheS)
+            
+        
+        for elmt in listeCourses:
+            self.ingredients.addItem(elmt)
+
+    def formatString(self,nomString):
         # Vire les espaces au debut et à la fin, vire les tirets et les
         # underscores. Et met en minuscule
         if (nomString is not None):
@@ -52,7 +111,7 @@ class Yara(QtWidgets.QMainWindow):
 
         return nomString
 
-    def cleanIngredients(ingr):
+    def cleanIngredients(self,ingr):
         for mesure in listMesures:
             ingr = ingr.replace(mesure,' ')
         for article in listeArticles:
@@ -61,7 +120,7 @@ class Yara(QtWidgets.QMainWindow):
         cleaningr = cleaningr.strip()
         return cleaningr
 
-    def getMarmiton(codeRecette):
+    def getMarmiton(self,codeRecette):
         response = requests.get(codeRecette)
         soup = BeautifulSoup(response.content,"html.parser")
         scriptvar = soup.find_all('script')
@@ -87,7 +146,7 @@ class Yara(QtWidgets.QMainWindow):
                     ListmIngredients.append(mIngredient)
         return ListmIngredients
 
-    def getKonbini(codeRecette):
+    def getKonbini(self,codeRecette):
         response = requests.get(codeRecette)
         soup = BeautifulSoup(response.content,"html.parser")
         listElements = soup.find_all('li',attrs={'class': None})
@@ -96,7 +155,7 @@ class Yara(QtWidgets.QMainWindow):
             ListmIngredients += ingredient
         return ListmIngredients
 
-    def getSlate(codeRecette):
+    def getSlate(self,codeRecette):
         response = requests.get(codeRecette)
         soup = BeautifulSoup(response.content,"html.parser")
         listElements = soup.find_all('li',attrs={'class': None})
@@ -105,7 +164,7 @@ class Yara(QtWidgets.QMainWindow):
             ListmIngredients += ingredient
         return ListmIngredients
 
-    def getMrCuisine(codeRecette):
+    def getMrCuisine(self,codeRecette):
         response = requests.get(codeRecette)
         soup = BeautifulSoup(response.content,"html.parser")
         globalElements = soup.findAll('div', attrs={'class':'recipe--ingredients-html-item col-md-8'})
@@ -116,23 +175,23 @@ class Yara(QtWidgets.QMainWindow):
                 ListmIngredients += ingredient
         return ListmIngredients
 
-    def FindIngredient(recette):
+    def FindIngredient(self,recette):
         if 'marmiton' in recette:
-            ListmIngredients = getMarmiton(recette)
+            ListmIngredients = self.getMarmiton(recette)
             
         if 'konbini' in recette:
-            ListmIngredients = getKonbini(recette)
+            ListmIngredients = self.getKonbini(recette)
         
         if 'slate' in recette:
-            ListmIngredients = getSlate(recette)
+            ListmIngredients = self.getSlate(recette)
 
         if 'monsieur-cuisine' in recette:
-            ListmIngredients = getMrCuisine(recette)
+            ListmIngredients = self.getMrCuisine(recette)
 
         addUnique = True
         for uniqueIngr in ListmIngredients:
-            uniqueIngr = cleanIngredients(uniqueIngr)
-            uniqueIngr = formatString(uniqueIngr)
+            uniqueIngr = self.cleanIngredients(uniqueIngr)
+            uniqueIngr = self.formatString(uniqueIngr)
             havetoadd = True
             for elmt in listeCourses:
                 if elmt in uniqueIngr:
